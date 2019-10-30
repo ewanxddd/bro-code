0687d85d038801d9a6d89e7ca5c04abb65f87b28
Ewan <vulcanv6@hotmail.com>
2019-10-30 17:57:32 +0000

    2 + 2 is four minus one thats 3 quick maths

--- num.py
+++ num.py
@@ -1,6 +1,6 @@
 print("add two numbers: ")
 
-num1 = int(input("Enter number 1: ")
-num2 = int(input("Enter number 2: ")
+num1 = int(input("Enter number 1: "))
+num2 = int(input("Enter number 2: "))
 
 print("Answer is: "num1 + num2)
